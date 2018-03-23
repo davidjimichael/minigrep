890222c4855e586184704c45fb0ad2f996dc95c0
David Michael <davidjimichael@gmail.com>
2018-03-23 13:11:33 -0400

    version1

--- source/qm/venv/hwk.py
+++ source/qm/venv/hwk.py
@@ -102,84 +102,135 @@ class LogicExpression:
             print(k, self.truthtable[k])
 
     def simplify(self):
-        # uncomment before later
-        # test = set(self.truthtable[self.statement])
-        # if len(test) == 1:
-        #     return "T" if test.pop() else "F"
-
-        minterms = []
+        new = set()
         terms = set()
-        #  create minterms list and the counts of the ones in the binary string
-        for x in range(len(self.truthtable[self.statement])):
-            if self.truthtable[self.statement][x]:
-                binstr = format(x, "0"+str(len(self.predicates))+"b")
-                ones = binstr.count("1")
-                minterms.append({"binary": binstr, "ones": ones})
-                terms.add(binstr)
-
         table = dict()
         removed = set()
-        for i in range(len(self.predicates)+1):
-            table.update({i: [m["binary"] for m in minterms if m["ones"] == i]})
-
-        keys = sorted(table.keys())
         simplified = False
-        for v in table.values():
-            for x in v:
-                terms.add(x)
-        count = lambda a, b: len([i for i in range(a) if a[i] != b[i]])
+        minterms = self.minterms()
+        datasize = int(math.log(len(minterms), 2))
 
+        for i in range(datasize+1):
+            table.update({i: [m for m in minterms if m.count("1") == i]})
+
+        for i in range(datasize+1):
+            for b in table[i]:
+                for p in table[i+1]:
+
+
+
+        print(table)
         while not simplified:
-            # for all the min terms
-            for ki, k in enumerate(keys):
-                # find next key
-                nextkey = keys[ki + 1 if ki < len(keys)-1 else ki]
-                for m in table[k]:
-                    # reference needed to check comparisons
-                    count = 0
-                    # enumerate the minterms
-                    # for all the bits
-                    for bi, b in enumerate(m):
-                        # if they're a 0
-                        if b == "0":
-                            # increase comparisons by 1
-                            count += 1
-                            # create a possible match to minterm in next set
-                            match = m[:bi] + '1' + m[bi+1:]
-                            # if match exists (in next group)
-                            if match in table[nextkey]:
-                                # create the combo of these
-                                dontcare = m[:bi] + "X" + m[bi + 1:]
-                                # add them both to used set
-                                print(m, match, dontcare)
-                                removed.add(m)
-                                removed.add(match)
-                                # add new term to newterms set
-                                terms.add(dontcare)
+            simplified = True
 
-            temp = terms.copy()
-            # for all combos
-            for i, d in enumerate(temp):
-                # for each letter in the combo
-                for j, c in enumerate(d):
-                    # if we have a "care" value and another term has a dont care there
-                    dc = next((x for x in temp if x[j]=="X"), None)
-                    if not (c == "X" or dc == None):
-                        # combine the two terms then the combo will be X unless both terms have same value
-                        # in position at this index or j in this case
-                        combo = ""
-                        for k in range(len(d)):
-                            if ((d[k] == "X" or dc[k] == "X") or d[k] != dc[k]):
-                                combo += "X"
-                            else:
-                                combo += d[k]
-                        removed.add(d)
-                        removed.add(dc)
-                        simplified = False
-                        terms.add(combo)
-                        print(d, dc, combo, "terms", terms)
+    def combine(self, a, b):
+        x = lambda a, b: a != b or a=="X" or b=="X"
+        fluxbits = [i for i in range(len(a)) if x(a[i], b[i])]
 
-        return terms.remove(removed)
+        if len(fluxbits) == 1:
+            return ''.join(["X" if x(a[i], b[i]) else a[i] for i in range(len(a))])
+        else:
+            return None
+
+    def minterms(self, n=None):
+        """
+        Returns a list of string formatted minterms. This will be
+        all possible 2^n combinations or the minterms for which
+        this logic statement is true.
+
+        :param n: possible number of predicates in statement
+        :return: minterms: a list of minterms formatted in a string
+        """
+        minterms = []
+        if n == None:
+            minterms = [i for i, x in enumerate(self.truthtable[self.statement]) if x == 1]
+        else:
+            minterms = [i for i in range(2**n)]
+
+        bitsize = str(math.ceil(math.log(len(minterms), 2)))
+        minterms = [format(x, "0" + bitsize + "b") for x in minterms]
+
+        return minterms
+
+    # def simplify(self):
+    #     # uncomment before later
+    #     # test = set(self.truthtable[self.statement])
+    #     # if len(test) == 1:
+    #     #     return "T" if test.pop() else "F"
+    #
+    #     minterms = []
+    #     terms = set()
+    #     #  create minterms list and the counts of the ones in the binary string
+    #     for x in range(len(self.truthtable[self.statement])):
+    #         if self.truthtable[self.statement][x]:
+    #             binstr = format(x, "0"+str(len(self.predicates))+"b")
+    #             ones = binstr.count("1")
+    #             minterms.append({"binary": binstr, "ones": ones})
+    #             terms.add(binstr)
+    #
+    #     table = dict()
+    #     removed = set()
+    #     for i in range(len(self.predicates)+1):
+    #         table.update({i: [m["binary"] for m in minterms if m["ones"] == i]})
+    #
+    #     keys = sorted(table.keys())
+    #     simplified = False
+    #     for v in table.values():
+    #         for x in v:
+    #             terms.add(x)
+    #     count = lambda a, b: len([i for i in range(a) if a[i] != b[i]])
+    #
+    #     while not simplified:
+    #         # for all the min terms
+    #         for ki, k in enumerate(keys):
+    #             # find next key
+    #             nextkey = keys[ki + 1 if ki < len(keys)-1 else ki]
+    #             for m in table[k]:
+    #                 # reference needed to check comparisons
+    #                 count = 0
+    #                 # enumerate the minterms
+    #                 # for all the bits
+    #                 for bi, b in enumerate(m):
+    #                     # if they're a 0
+    #                     if b == "0":
+    #                         # increase comparisons by 1
+    #                         count += 1
+    #                         # create a possible match to minterm in next set
+    #                         match = m[:bi] + '1' + m[bi+1:]
+    #                         # if match exists (in next group)
+    #                         if match in table[nextkey]:
+    #                             # create the combo of these
+    #                             dontcare = m[:bi] + "X" + m[bi + 1:]
+    #                             # add them both to used set
+    #                             print(m, match, dontcare)
+    #                             removed.add(m)
+    #                             removed.add(match)
+    #                             # add new term to newterms set
+    #                             terms.add(dontcare)
+    #
+    #         temp = terms.copy()
+    #         # for all combos
+    #         for i, d in enumerate(temp):
+    #             # for each letter in the combo
+    #             for j, c in enumerate(d):
+    #                 # if we have a "care" value and another term has a dont care there
+    #                 dc = next((x for x in temp if x[j]=="X"), None)
+    #                 if not (c == "X" or dc == None):
+    #                     # combine the two terms then the combo will be X unless both terms have same value
+    #                     # in position at this index or j in this case
+    #                     combo = ""
+    #                     for k in range(len(d)):
+    #                         if ((d[k] == "X" or dc[k] == "X") or d[k] != dc[k]):
+    #                             combo += "X"
+    #                         else:
+    #                             combo += d[k]
+    #                     removed.add(d)
+    #                     removed.add(dc)
+    #                     simplified = False
+    #                     terms.add(combo)
+    #                     print(d, dc, combo, "terms", terms)
+    #
+    #     return terms.remove(removed)
 
 
 if __name__ == "__main__":
---
+++ source/qm/venv/test.py
@@ -0,0 +1,69 @@
+import math
+
+def combine(a, b):
+    term = []
+
+    if a == b or diffcount(a, b) != 1:
+        return None
+
+    for i in range(len(a)):
+        if a[i] == b[i]:
+            term.append(a[i])
+        elif a[i]=="X" or b[i]=="X":
+            term.append("X")
+        else:
+            term.append("X")
+    return ''.join(term)
+
+def diffcount(a, b):
+    count = 0
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            if not (a[i]=="X" or b[i]=="X"):
+                count += 1
+    return count
+
+def minterms(n=0):
+    """
+    Returns a list of string formatted minterms. This will be
+    all possible 2^n combinations or the minterms for which
+    this logic statement is true.
+
+    :param n: possible number of predicates in statement
+    :return: minterms: a list of minterms formatted in a string
+    """
+    minterms = []
+    if False:
+        minterms = [i for i, x in enumerate(self.truthtable[self.statement]) if x == 1]
+    else:
+        minterms = [i for i in range(2**n)]
+
+    bitsize = str(math.ceil(math.log(len(minterms), 2)))
+    minterms = [format(x, "0" + bitsize + "b") for x in minterms]
+
+    return minterms
+
+m = minterms(3)
+s = False
+terms = set(m)
+p = len(terms)
+r = set()
+n = set()
+
+while not s:
+    for x in terms:
+        for y in terms:
+            z = combine(x, y)
+            if z != None:
+                r.add(x)
+                r.add(y)
+                n.add(z)
+    for v in r:
+        terms.remove(v)
+    for u in n:
+        terms.add(u)
+    s = len(r) == 0
+    r.clear()
+    n.clear()
+
+print(m, "=", terms)
